0fcd78df67631b14ef2163f3a469df428cd6cc2a
Charlie <charlie@vinecode.uk>
2022-01-11 16:35:16 +0000

    Fundamentals P1 - Lesson 10

---
+++ fundamentals-1/assignments.js
@@ -0,0 +1,18 @@
+/* 
+  Values and Variables
+  --------------------
+1 - Declare variables called 'country', 'continent' and 'population' and
+    assign their values according to your own country (population in millions)
+    
+2 - Log their values to the console
+
+*/
+
+
+let country = "United Kingdom";
+let continent = "Europe";
+let population = 746400000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ fundamentals-1/script.js
@@ -0,0 +1,32 @@
+// Lesson 10
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda"; 
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+/* 
+  Variable Notes 
+  --------------
+# Variable names can't start with numbers
+# Variable names can only use underscores and dollar signs (symbols)
+# To use new / function variables, use _ or $ before hand ($function)
+# Variable names should not start with capital letters
+# Variables all in uppercase are saved for those that'll never change, e.g. PI
+# Use good names for variables, myFirstJob/myCurrentJob over job1/job2
+*/
+
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
