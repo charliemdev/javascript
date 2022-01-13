30afebc0d2956897d14a4ea4f014694811fbb217
Charlie <charlie@vinecode.uk>
2022-01-13 14:51:49 +0000

    Fundamentals P1 - Lesson 13

--- fundamentals-1/script.js
+++ fundamentals-1/script.js
@@ -89,3 +89,39 @@ const birthYear = 1991;
 // birthYear = 1990;
 */
 
+// Lesson 13 - Basic Operators 
+/*
+  NOTE:
+  -----
+# 
+*/
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1998;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment operators 
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+console.log(x);
+
+// Comparison operators 
+console.log(ageJonas > ageSarah); // >, <, >=. <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
