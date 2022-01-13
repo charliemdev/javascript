27fe6c6103779604b657cddededed7b8c508b98b
Charlie <charlie@vinecode.uk>
2022-01-13 14:57:16 +0000

    Fundamentals P1 - Assignments

--- fundamentals-1/assignments.js
+++ fundamentals-1/assignments.js
@@ -1,6 +1,6 @@
 /* 
-  1 - Values and Variables
-  ------------------------
+  Values and Variables
+  --------------------
 1 - Declare variables called 'country', 'continent' and 'population' and
     assign their values according to your own country (population in millions)
     
@@ -17,8 +17,8 @@ console.log(population); */
 
 
 /* 
-  2 - Data Types
-  --------------
+  Data Types
+  ----------
 1 - Declare a variable called 'isIsland' and set its value according to your
     country. The variable should hold a Boolean value. Also declare a variable
     'language', but don't assign it any value yet
@@ -37,8 +37,8 @@ console.log(typeof language); */
 
 
 /*
-  3 - let, const and var
-  ----------------------
+  let, const and var
+  ------------------
 1 - Set the value of 'language' to the language spoken where you live (some
     countries have multiple languages, but just choose one)
     
@@ -48,10 +48,15 @@ console.log(typeof language); */
 3 - Try to change one of the changed variables now, and observe what happens
 */
 
+/* language = "English";
+const country = "United Kingdom";
+const continent = "Europe";
+const isIsland = true;
+isIsland = false; */
 
 /*
-  4 - Basic Operations
-  --------------------
+  Basic Operations
+  ----------------
 1 - If your country split in half, and each half would contain half the population,
     then how many people would live in each half?
     
@@ -67,3 +72,11 @@ console.log(typeof language); */
     which contains a string with this format: 'Portugal is in Europe, and its 11 million
     people speak Portuguese'.
 */
+
+/* console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description1 = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description1); */
